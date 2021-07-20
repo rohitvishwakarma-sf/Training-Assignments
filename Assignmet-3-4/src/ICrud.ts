7632e96622c012febea46328009864728d69df75
rohitvishwakarma-sf <rohit.vishwakarma@sourcefuse.com>
2021-07-20 16:20:56 +0530

    PR changes done

--- Assignmet-3-4/src/ICrud.ts
+++
@@ -1,9 +0,0 @@
-
-
-export interface ICrud {
-    create(hostEle: HTMLElement, editHandler: EventListenerOrEventListenerObject, deleteHandler: EventListenerOrEventListenerObject, saveHandler: EventListenerOrEventListenerObject, cancelHandler: EventListenerOrEventListenerObject): void;
-    delete(): void;
-    edit(): void;
-    save(): void;
-    cancel(): void;
-}
--- Assignmet-3-4/src/app.ts
+++ Assignmet-3-4/src/app.ts
@@ -1,57 +1,52 @@
-import { TableView } from './tableView';
-import { autoBind } from "./decorators/autobind";
+import { UserCrud } from "./UserCrud";
+import { autoBind } from "./decorators/decorator";
 
 function dateTimeDC(target: any, propertyKey: string) {
-    const update = Reflect.defineProperty(
-        target,
-        propertyKey, {
-        get: () => {
-
-            return new Date().toLocaleString();
-        },
-        set: (value: string) => {
-
-        }
-    });
+  const update = Reflect.defineProperty(target, propertyKey, {
+    get: () => {
+      return new Date().toLocaleString();
+    },
+    set: (value: string) => {},
+  });
 }
 class HomePage {
-    loadButton: HTMLButtonElement;
-    refreshButton: HTMLButtonElement;
-    tableView: TableView;
-    @dateTimeDC
-    date: string = "";
+  loadButton: HTMLButtonElement;
+  refreshButton: HTMLButtonElement;
+  userCrud: UserCrud;
+  @dateTimeDC
+  date: string = "";
 
-    constructor() {
-        this.loadButton = document.getElementById("loadbutton")! as HTMLButtonElement;
-        this.refreshButton = document.getElementById("refreshbutton")! as HTMLButtonElement;
-        this.tableView = new TableView("tablecontainer")
+  constructor() {
+    this.loadButton = document.getElementById(
+      "loadbutton"
+    )! as HTMLButtonElement;
+    this.refreshButton = document.getElementById(
+      "refreshbutton"
+    )! as HTMLButtonElement;
+    this.userCrud = new UserCrud("tablecontainer");
 
-        this.loadButton.addEventListener("click", this.loadData);
-        this.refreshButton.addEventListener("click", this.refreshData);
-        this.refreshButton.style.display = "none";
-        // this.loadData();
-    }
+    this.loadButton.addEventListener("click", this.loadData);
+    this.refreshButton.addEventListener("click", this.refreshData);
+    this.refreshButton.style.display = "none";
+    // this.loadData();
+  }
 
-    updateDate() {
-        const datetime = document.getElementById("datetime");
-        datetime!.innerHTML = `Updated on - ${this.date.replace(",", " at ")}`;
-    }
+  updateDate() {
+    const datetime = document.getElementById("datetime");
+    datetime!.innerHTML = `Updated on - ${this.date.replace(",", " at ")}`;
+  }
 
-    @autoBind
-    loadData(): void {
-        this.tableView.load();
-        this.refreshButton.style.display = "";
-        this.loadButton.style.display = "none";
-        this.updateDate();
-
-
-
-    }
-    @autoBind
-    refreshData(): void {
-        this.tableView!.refresh();
-        this.updateDate();
-
-    }
+  @autoBind
+  loadData(): void {
+    this.userCrud.load();
+    this.refreshButton.style.display = "";
+    this.loadButton.style.display = "none";
+    this.updateDate();
+  }
+  @autoBind
+  refreshData(): void {
+    this.userCrud!.refresh();
+    this.updateDate();
+  }
 }
 new HomePage();
